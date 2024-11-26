1d6f1bebbd3b16d0d7c7c74b43abeba5e0c8b1f1
KethelyTS <kethelytavares0@gmail.com>
2024-11-26 20:39:07 -0300

    app-routing.module.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,21 @@
+app-routing.module.ts
+@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlayerComponent } from './components/player/player.component';
+import { AuthGuard } from './services/auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'player', component: PlayerComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
